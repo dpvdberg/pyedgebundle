3b2608872f29f61ba14d48738535d9be077e824f
Sophie <s.v.d.eerenbeemt@student.tue.nl>
2021-03-08 16:40:34 +0100

    getCandidateNeighbours() of Ant.py

--- Datastructures/Ant.py
+++ Datastructures/Ant.py
@@ -22,18 +22,43 @@ class Ant:
         self.direction = self.direction + newDirec
 
     def getLeftAntenna(self):
-        return (round(self.location[0] + math.cos(self.direction + math.pi/4)),
-                round(self.location[1] + math.sin(self.direction + math.pi/4)))
+        return self.calcPixel(self.direction + math.pi/4)
 
     def getRightAntenna(self):
-        return (round(self.location[0] + math.cos(self.direction + math.pi / 4)),
-                round(self.location[1] + math.sin(self.direction + math.pi / 4)))
+        return self.calcPixel(self.direction - math.pi/4)
 
     def getCandidateNeighbours(self) -> List:
-        pass
+        neighbours = []
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                neighbours.append((self.location[0] + i, self.location[1] + j))
+
+        # Only use neighbours that have not been visited before
+        nvisited = [x for x in neighbours if x not in self.path]
+        if not nvisited:
+            return neighbours
+
+        # Only consider neighbours that are not further away from the goal
+        angleEnd = self.calcAngle(self.goal)
+        # TODO: optimize using modulo operation instead of arctan
+        nborder = [x for x in nvisited if -math.pi * 3 / 4 < math.atan2(math.sin(angleEnd - self.calcAngle(x)), math.cos(angleEnd - self.calcAngle(x))) < math.pi * 3 / 4 ]
+        if not nborder:
+            return nvisited
+
+        # Only consider neighbours that make sharp turns w.r.t. our current position
+        nfinal = [x for x in nvisited if -math.pi / 2 <= math.atan2(math.sin(self.direction - self.calcAngle(x)), math.cos(self.direction - self.calcAngle(x))) <= math.pi / 2]
+        if not nfinal:
+            return nborder
+        return nfinal
 
     def takeStep(self):
-        newLoc = (round(self.location[0] + math.cos(self.direction)),
-                     round(self.location[1] + math.sin(self.direction)))
+        newLoc = self.calcPixel(self.direction)
         self.location = newLoc
         self.addToPath(newLoc)
+
+    def calcPixel(self, direction):
+        return (round(self.location[0] + math.cos(direction)),
+                     round(self.location[1] + math.sin(direction)))
+
+    def calcAngle(self, goal):
+        return math.atan2(goal[1] - self.location[1], goal[0] - self.location[0])
