dbc9540aeab061519a9643e5ffd1c20c239f31bc
Dennis van den Berg <d.p.v.d.berg@student.tue.nl>
2021-03-10 13:26:45 +0100

    Add form

---
+++ ui/pyedgebundleUI/main.py
@@ -0,0 +1,59 @@
+import sys
+from xml.etree import ElementTree
+
+import easygui
+import networkx as nx
+from PySide2 import QtWidgets
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import *
+from PySide2.QtCore import QFile, Qt
+
+import matplotlib
+
+from parser.GraphUtils import GraphUtils
+from ui.EBCanvas import EBCanvas
+
+matplotlib.use('Qt5Agg')
+
+
+class Form(QMainWindow):
+    def __init__(self, ui_file, parent=None):
+        super(Form, self).__init__(parent)
+        ui_file = QFile(ui_file)
+        ui_file.open(QFile.ReadOnly)
+
+        loader = QUiLoader()
+        self.ui = loader.load(ui_file)
+        ui_file.close()
+
+        # Menu bar
+        self.ui.menuFileOpen.triggered.connect(self.openFile)
+
+        # Matplotlib widget
+        mplWidget = self.ui.findChild(QWidget, 'mplWidget')
+        layout = QVBoxLayout(mplWidget)
+
+        self.canvas = EBCanvas()
+        self.ui.setCentralWidget(self.canvas)
+
+        self.ui.show()
+
+    def openFile(self):
+        path = easygui.fileopenbox()
+
+        try:
+            g = nx.read_graphml(path)
+        except (nx.NetworkXError, ElementTree.ParseError) as e:
+            error_dialog = QtWidgets.QErrorMessage()
+            error_dialog.showMessage(str(e))
+
+            error_dialog.exec_()
+            return
+
+        g, success = GraphUtils.sanitize(g)
+        print(success)
+
+if __name__ == '__main__':
+    app = QApplication()
+    form = Form('form.ui')
+    sys.exit(app.exec_())
